73008364b49b5d84a34df1931b7f7cfc192f4724
Conor James Giles <cgiles290@gmail.com>
2018-04-15 17:14:06 +0100

    JUnit5 Tests

---
+++ src/tst/DBITest.java
@@ -0,0 +1,57 @@
+package tst;
+import routes.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.Map.Entry;
+import java.util.concurrent.ExecutionException;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+class DBITest {
+	DBI dbi;
+	Graph graph;
+	
+	public static class AsNonApp extends Application {
+	    @Override
+	    public void start(Stage primaryStage) throws Exception {
+	    }
+	}
+
+	@BeforeAll
+	public static void initJFX() {
+	    Thread t = new Thread("JavaFX Init Thread") {
+	        public void run() {
+	            Application.launch(AsNonApp.class, new String[0]);
+	        }
+	    };
+	    t.setDaemon(true);
+	    t.start();
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+		dbi = new DBI("test.osm");
+		graph = null;
+	}
+
+	@Test
+	void testCall() {
+		dbi.run();
+		try {
+			graph = dbi.get();
+		} catch (InterruptedException | ExecutionException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		assertArrayEquals(new String[]{"0", "1", "2", "3", "4", "5", "6", "7"},graph.nodes.keySet().toArray());
+		for(Entry<String,Graph.Node> e : graph.nodes.entrySet())
+			assertNotNull(e.getValue());
+	}
+
+}
---
+++ src/tst/GraphRelationTest.java
@@ -0,0 +1,31 @@
+package tst;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.platform.runner.JUnitPlatform;
+import org.junit.runner.RunWith;
+
+public class GraphRelationTest {
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@Test
+	void testTag() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testRelation() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testMember() {
+		fail("Not yet implemented");
+	}
+
+}
