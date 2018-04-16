78234f403c5dcaa705662e3a9a98347e9e130b57
Andrew <sn1perandr3w@gmail.com>
2018-04-16 14:07:27 +0100

    DJK Work

--- src/routes/DJK.java
+++ src/routes/DJK.java
@@ -6,40 +6,55 @@ import routes.Graph.Node;
 public class DJK {
 
 	Node startPoint;
+	Node currentNode;
 	Node endPoint;
+	Node nextNode;
 	Graph graph;
-	
-	public DJK(Graph graph)
-	{
-		
+
+	public DJK(Graph graph) {
+
 		this.graph = getGraph(graph);
-		
+
 	}
-	
-	public void DJKSEARCH(Node selStartPoint, Node selEndPoint) 
-	{
+
+	public void DJKSEARCH(Node selStartPoint, Node selEndPoint) {
 		System.out.println("DJK NODES ASSIGNED");
 		this.startPoint = selStartPoint;
-		this. endPoint = selEndPoint;
+		this.endPoint = selEndPoint;
+		this.currentNode = startPoint;
 		dijkstra();
 	}
-	
-	
-	public Graph getGraph(Graph graph) 
-	{
+
+	public Graph getGraph(Graph graph) {
 		return graph;
 	}
-	
-	public void dijkstra() 
-	{
+
+	public void dijkstra() {
+		
+		
+		
 		System.out.println(startPoint);
 		System.out.println(endPoint);
-		
 		System.out.println(graph);
-		/*
-		for (String key : graph.nodes.keySet()) {
-		    System.out.println("Key = " + key);
+
+		
+		ArrayList priorityQueue = new ArrayList<Node>();
+		ArrayList nodesVisited = new ArrayList<Node>();
+
+		
+		while(currentNode != endPoint) {
+			for (Graph.Way way : currentNode.ways) {
+				if (way.nds.get(0) == currentNode) {
+					System.out.println("Current node.");
+					System.out.println(currentNode.id);
+					System.out.println(way.id + ": " + way.nds.get(0) + " -> " + way.nds.get(way.nds.size() - 1));
+					currentNode = way.nds.get(way.nds.size() - 1);
+				}
+			}
+			
 		}
-		*/
+		System.out.println("ENDPOINT");
+		System.out.println(currentNode.id);
+
 	}
 }
